fb90a5c3517162b28d98d45f45fd8a3da600c050
ht_cheng <ht_cheng@phison.com>
2020-04-21 17:24:28 +0800

    Initial project.

---
+++ FormatTool/FormatToolDlg.h
@@ -0,0 +1,33 @@
+﻿
+// FormatToolDlg.h: 標頭檔
+//
+
+#pragma once
+
+
+// CFormatToolDlg 對話方塊
+class CFormatToolDlg : public CDialogEx
+{
+// 建構
+public:
+	CFormatToolDlg(CWnd* pParent = nullptr);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FORMATTOOL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
